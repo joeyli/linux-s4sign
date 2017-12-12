bae39460393ada4c0226dd07cd5e3afcef86b71f
Lee, Chun-Yi <jlee@suse.com>
2017-12-12 16:37:58 +0800

    PM / hibernate: Generate and verify signature for snapshot image
    When producing memory snapshot image, hibernation uses HMAC-SHA512
    with EFI secret key to calculate the hash for all data pages in image.
    The hash result will be kept in the snapshot header as the image
    signature. Before hibernation restores system, kernel executes
    HMAC-SHA512 again and compares the result with the signature in header
    to verify the integrity of snapshot image.
    
    If the verification failed, the resume process will be stopped. The
    snapshot image will be discarded and system will boot as normal.
    
    Signed-off-by: Lee, Chun-Yi <jlee@suse.com>

--- kernel/power/swap.c
+++ kernel/power/swap.c
@@ -1096,7 +1096,8 @@ static int load_image(struct swap_map_handle *handle,
 		snapshot_write_finalize(snapshot);
 		if (!snapshot_image_loaded(snapshot))
 			ret = -ENODATA;
-
+		if (!ret)
+			ret = snapshot_image_verify();
 		snapshot_init_trampoline();
 		/* clean the hidden area in boot kernel */
 		clean_hidden_area();
@@ -1452,6 +1453,8 @@ out_finish:
 				}
 			}
 		}
+		if (!ret)
+			ret = snapshot_image_verify();
 		snapshot_init_trampoline();
 		/* clean the hidden area in boot kernel */
 		clean_hidden_area();
